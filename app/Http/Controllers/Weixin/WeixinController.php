37136020c08360fc5e5148ed7a2c991cdb80bcde
zyd <zhaoyanda@163.com>
2019-02-19 10:56:06 +0800

    修改

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -148,14 +148,28 @@ class WeixinController extends Controller
         $client = new GuzzleHttp\Client(['base_uri' => $url]);
 
         $data = [
-            "button"    => [
+            "button"=>[
                 [
-                    "type"  => "view",      // view类型 跳转指定 URL
-                    "name"  => "Lening222",
-                    "url"   => "https://www.baidu.com"
-                ]
-            ]
+                    "type"=>"click",
+                    "name"=>"今日歌曲",
+                    "key"=>"V1001_TODAY_MUSIC"
+                ],
+                [
+                    "name"=>"菜单",
+                    "sub_button"=>[
+                        [
+                            "type"=>"view",
+                            "name"=>"搜索",
+                            "url"=>"http://www.baidu.com/"
+                        ],
+                        [
+                            "type"=>"click",
+                            "name"=>"赞一下我们",
+                            "key"=>"V1001_GOOD"
+                        ]]
+                ]]
         ];
+
 
 
         $r = $client->request('POST', $url, [
