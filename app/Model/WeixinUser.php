27e98ea16c72dccd50ad486c094679252bdd1f36
zyd <zhaoyanda@163.com>
2019-02-19 10:13:19 +0800

    修改

---
+++ app/Model/WeixinUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class WeixinUser extends Model
+{
+    //
+    public $table = 'laravel';
+    public $timestamps = false;
+
+}
