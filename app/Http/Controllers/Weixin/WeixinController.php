9ef16c5047bae88ed39011519e8f197db46cc4ce
zyd <zhaoyanda@163.com>
2019-02-20 17:21:10 +0800

    wx

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -54,14 +54,7 @@ class WeixinController extends Controller
         // 处理用户发送消息
         if ($xml->MsgType == 'text') {            //用户发送文本消息
             $msg = $xml->Content;
-            $xml_response = '
-                 <xml>
-                 <ToUserName><![CDATA[' . $openid . ']]></ToUserName>
-                 <FromUserName><![CDATA[' . $xml->ToUserName . ']]></FromUserName>
-                 <CreateTime>' . time() . '</CreateTime>
-                 <MsgType><![CDATA[text]]></MsgType>
-                 <Content><![CDATA[' . $msg . date('Y-m-d H:i:s') . ']]></Content>
-                 </xml>';
+            $xml_response = '<xml><ToUserName><![CDATA[' . $openid . ']]></ToUserName><FromUserName><![CDATA[' . $xml->ToUserName . ']]></FromUserName><CreateTime>' . time() . '</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[' . $msg . date('Y-m-d H:i:s') . ']]></Content></xml>';
             echo $xml_response;
         }else if ($xml->MsgType == 'image') {       //用户发送图片信息
             //视业务需求是否需要下载保存图片
