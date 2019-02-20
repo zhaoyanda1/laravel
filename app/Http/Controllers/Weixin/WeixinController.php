c5ca770277bb92381da761e437eb1782219d42b2
zyd <zhaoyanda@163.com>
2019-02-20 17:33:46 +0800

    wx

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -47,7 +47,7 @@ class WeixinController extends Controller
                 $msg = $xml->Content;
                 $xml_response = '<xml><ToUserName><![CDATA['.$openid.']]></ToUserName><FromUserName><![CDATA['.$xml->ToUserName.']]></FromUserName><CreateTime>'.time().'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA['. $msg. date('Y-m-d H:i:s') .']]></Content></xml>';
                 echo $xml_response;
-                exit();
+                //exit();
             }elseif($xml->MsgType=='image'){       //用户发送图片信息
                 //视业务需求是否需要下载保存图片
                 if(1){  //下载图片素材
@@ -294,7 +294,7 @@ class WeixinController extends Controller
 
         }
     }
-    
+
     public function all()
     {
         $access_token = $this->getWXAccessToken();
