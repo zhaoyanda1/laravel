6c267c7d5f0725dd97d5d34210f6d9a07cc5e213
zyd <zhaoyanda@163.com>
2019-02-20 18:39:43 +0800

    sdasdasddsdsassasadasd

--- app/Http/Controllers/Weixin/WeixinController.php
+++ app/Http/Controllers/Weixin/WeixinController.php
@@ -289,7 +289,7 @@ class WeixinController extends Controller
         $url = 'https://api.weixin.qq.com/cgi-bin/user/info?access_token=' . $access_token . '&openid=' . $openid . '&lang=zh_CN';
 
         $data = json_decode(file_get_contents($url), true);
-        //echo '<pre>';print_r($data);echo '</pre>';
+        echo '<pre>';print_r($data);echo '</pre>';
         return $data;
     }
 
