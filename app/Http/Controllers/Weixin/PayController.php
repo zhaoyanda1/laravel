1b7a51f9423670b977b06b69f272a94ab7ac8ab4
zyd <zhaoyanda@163.com>
2019-02-27 09:48:31 +0800

    d

--- app/Http/Controllers/Weixin/PayController.php
+++ app/Http/Controllers/Weixin/PayController.php
@@ -59,7 +59,7 @@ class PayController extends Controller
         //echo '<pre>';print_r($data);echo '</pre>';
 
         //将 code_url 返回给前端，前端生成 支付二维码
-        return view('weixin.pay',['code_url'=>$data->code_url]);
+        return view('weixin.pay',['code_url'=>$data->code_url],['title','支付页面']);
     }
 
 
